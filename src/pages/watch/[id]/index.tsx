5d020a1389a48df83dcc11a8449bd110c29e7d28
isaqueks <iskschluter@gmail.com>
2022-05-03 09:17:22 -0300

    fix scroll and ass 720p

--- src/pages/watch/[id]/index.tsx
+++ src/pages/watch/[id]/index.tsx
@@ -6,6 +6,9 @@ import DeathNoteSeries from "@ts/series/deathNote";
 import style from '@styles/DeathNote.module.css';
 import Meta from "@components/Meta";
 import { AnimeEpisode } from "@ts/series";
+import ScrollableSection from "@components/scrollableSection";
+import ScollableViewport from "@components/scrollableViewport";
+import ScrollableViewport from "@components/scrollableViewport";
 
 export const getStaticPaths = async () => {
 
@@ -43,25 +46,27 @@ interface Props {
 export default function DeathNoteAnime({ episode, index }: Props) {
 
     return (<div>
-        <Meta 
-            title={`Death Note EP ${index+1} - ${episode.title} (Dublado) - DeathNoteBR`}
+        <Meta
+            title={`Death Note EP ${index + 1} - ${episode.title} (Dublado) - DeathNoteBR`}
             keywords={"Death Note,Anime,Dublado,".concat(episode.title)}
-            description={`Assista Death Note online Dublado - Episódio ${index+1} (${episode.title}) - DeathNoteBR - Comunidade Brasileira - L vs Kira - Assistir Death Note gratuitamente`} 
-            type="video.episode" 
+            description={`Assista Death Note online Dublado - Episódio ${index + 1} (${episode.title}) - DeathNoteBR - Comunidade Brasileira - L vs Kira - Assistir Death Note gratuitamente`}
+            type="video.episode"
             url={`https://deathnote.com.br/watch/${index}`}
         >
             <meta property="og:video:series" content="Death Note" />
         </Meta>
-        <div className={`${style.deathNote} ${style.pageBlock}`}>
-            <Navbar title={episode.title} />
-            <AnimePlayer currentEpisode={index} series={DeathNoteSeries} />
-            <Padding>
-                <CommentSection
-                    title={`${episode.title} - Death Note - DeathNoteBR`}
-                    identifier={episode.title}
-                />
-            </Padding>
-        </div>
+        <ScrollableViewport>
+            <div className={`${style.deathNote} ${style.pageBlock}`}>
+                <Navbar title={episode.title} />
+                <AnimePlayer currentEpisode={index} series={DeathNoteSeries} />
+                <Padding>
+                    <CommentSection
+                        title={`${episode.title} - Death Note - DeathNoteBR`}
+                        identifier={episode.title}
+                    />
+                </Padding>
+            </div>
+        </ScrollableViewport>
     </div>)
 
 }
--- src/ts/series/deathNote.FHD.ts
+++ src/ts/series/deathNote.FHD.ts
@@ -6,334 +6,334 @@ const DeathNoteSeries: AnimeSeries = {
         {
             title: "Episódio 1 - Renascimento",
             description: 'O brilhante, mas entediado aluno do ensino médio Light Yagami descobre de repente que tem o poder da vida e da morte em suas mãos.',
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_1.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_1.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 2 - Confronto",
             description: 'Enquanto os criminosos do mundo continuam morrendo, L se reúne por computador com os representantes de várias nações.',
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_2.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_2.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 3 - Negociações",
             description: "Ryuk, o Shinigami, ou deus da morte, que mandou a Death Note para o mundo humano, alerta Light de que alguém está atrás dele.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_3.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_3.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 4 - Perseguição",
             description: "Light – agora conhecido no mundo como Kira – testa a Death Note para compreender o seu poder e mata seis presos de várias formas...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_4.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_4.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 5 - Táticas",
             description: "Soichiro Yagami exige um encontro pessoalmente com L quando descobre que L trouxe agentes do FBI secretamente para o Japão.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_5.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_5.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 6 - Rasgo",
             description: "Light encontra uma mulher no Departamento de Polícia com uma mensagem urgente para a força-tarefa de Kira.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_6.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_6.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 7 - Céu nublado",
             description: "Light usa a Death Note para tentar se livrar da mulher que poderia capturá-lo, e fica chocado quando isso não funciona.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_7.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_7.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 8 - Ofuscação",
             description: "L insiste em colocar escutas e câmeras nas casas do Chefe de Política Yagami e do Delegado Kitamura...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_8.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_8.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 9 - Contato",
             description: "Os assassinatos continuam e L começa a suspeitar do comportamento perfeito de Light e decide investigá-lo por conta própria.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_9.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_9.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 10 - Dúvida",
             description: "A suspeita de L em relação a Light aumenta, fazendo com que ele convide Light para a investigação.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_10.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_10.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 11 - Assalto",
             description: "Como previsto nas fitas enviadas para a Sakura TV, os comentadores e âncoras de notícias que expressaram dúvida sobre Kira morrem um a um.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_11.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_11.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 12 - Amor",
             description: "Com um segundo Kira agora à solta, L pede a Light para ingressar na força-tarefa e se comportar como Kira real a fim de descobrir quem é a \"cópia\".",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_12.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_12.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 13 - Confissões",
             description: "Light vai para Aoyama para descobrir a identidade do segundo Kira antes de L suspeitar ainda mais.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_13.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_13.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 14 - Amigo",
             description: "Light e Misa mostram um para o outro suas Death Notes, e Light pede a Misa para que seu shinigami Rem se livrar de L.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_14.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_14.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 15 - Aposta",
             description: "As evidências apontam para Misa Amane como o segundo Kira.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_15.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_15.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 16 - Decisão",
             description: "Misa está em confinamento, embora suas memórias da Death Note tenham sido totalmente apagadas.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_16.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_16.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 17 - Execução",
             description: "Depois de 50 dias de confinamento, as mortes continuam, e Misa e Light são soltos.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_17.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_17.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 18 - Aliado",
             description: "Embora L ainda tenha suspeitas, Light e Misa, com suas memórias de ser Kira apagadas, retornam para o quartel general...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_18.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_18.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 19 - Matsuda",
             description: "Matsuda, o membro da força-tarefa designado para vigiar Misa, está ansioso para se destacar...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_19.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_19.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 20 - Artifício",
             description: "A equipe de L descobre que o Yotsuba Group se reúne em segredo para discutir a eliminação de pessoas que são problemáticas para sua empresa...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_20.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_20.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 21 - Performance",
             description: "O Yotsuba Group contrata Eraldo Coil para descobrir a identidade de L.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_21.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_21.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 22 - Direção",
             description: "Misa tem sucesso ao reduzir os suspeitos de Kira no Yotsuba Group a um: Higuchi.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_22.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_22.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 23 - Frensei",
             description: "Higuchi do Yotsuba Group tenta matar Matsuda da força-tarefa, e fica surpreso quando ele não morre após 40 segundos.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_23.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_23.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 24 - Ressurreição",
             description: "Higuchi é capturado e conta aos investigadores sobre a Death Note.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_24.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_24.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 25 - Silêncio",
             description: "Light, com sua memória recuperada, diz a Misa para começar a matar depois da morte de Higuchi.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_25.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_25.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 26 - Renovação",
             description: "Light tirou todos de seu caminho e está mais próximo de atingir seu mundo perfeito.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_26.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_26.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 27 - Abdução",
             description: "Um novo investigador avança – Near, uma figura central na SPK, uma organização que está tentando pegar Kira sem contar com L.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_16.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_16.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 28 - Impaciência",
             description: "Light, agindo como L, revela para Near que sua irmã Sayu foi sequestrada por alguém querendo trocá-la pela Death Note.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_28.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_28.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 29 - Pai",
             description: "Light tenta descobrir onde Mello e seus associados da Máfia estão escondidos e usa os olhos de Misa para encontrar o dono atual da Death Note.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_29.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_29.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 30 - Justiça",
             description: "Mello entra no quartel general do SPK para recuperar uma fotografia dele, a fim de se certificar de que não será morto por Kira.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_30.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_30.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 31 - Transferência",
             description: "Near contata a força-tarefa japonesa e determina que Kira está entre seus membros.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_31.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_31.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 32 - Seleção",
             description: "O membro do elenco de estrelas, Kiyomi Takada, se torna o porta-voz de Kira para o mundo...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_32.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_32.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 33 - Desprezo",
             description: "Light finge investigar Kiyomi...",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_33.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_33.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 34 - Vigilância",
             description: "Aizawa descobre o que está acontecendo e conta a Near, que já sabe sobre as notas.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_34.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_34.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 35 - Malícia",
             description: "Light dá a Kiyomi a ordem para acabar com Near.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_35.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_35.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 36 - 1.28",
             description: "Near, junto com o SPK, encontra Light e a força-tarefa em um depósito para o tiroteio final.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_36.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_36.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
         {
             title: "Episódio 37 - Novo Mundo",
             description: "Embora Mikami tenha escrito todos os nomes dos investigadores na Death Note, um minuto se passa e todos ainda estão vivos.",
-            videoUrl: "https://static.deathnote.com.br/720p/death_note_37.mp4",
+            videoUrl: "https://static.deathnote.com.br/death_note_37.mp4",
             resolution: {
-                width: 1280,
-                height: 720
+                width: 1920,
+                height: 1080
             }
         },
 
